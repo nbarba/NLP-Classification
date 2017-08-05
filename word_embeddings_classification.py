69e280a0aa8aad5063a055765f47cecef4e4aada
nbarba <nbarba@nbarbas-MacBook-Pro.local>
2017-08-04 23:26:27 -0700

    Updated main arguments

--- word_embeddings_classification.py
+++ word_embeddings_classification.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
 import sys, getopt
@@ -10,7 +9,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 import math
 import os.path
-import urllib2
+import argparse
+
 
 class EmbeddingFeaturesGenerator:
 
@@ -99,31 +99,39 @@ class EmbeddingFeaturesGenerator:
 		return X,Y
 
 
-def main():
+def main(train_set,test_set,embeddings_file):
+	
+	if not os.path.exists(embeddings_file):
+		print "Download pre-trained word embeddings...."
+		os.system('wget https://www.dropbox.com/s/h87tstu4awtvgew/wiki_embeddings.txt')
 
-	inputfile=sys.argv[1:][0];
-	train_set=sys.argv[1:][1];
-	test_set=sys.argv[1:][2];
+	feature_gen=EmbeddingFeaturesGenerator(embeddings_file);
 
+	print "-------- Feature Extraction --------";
 
-	if not os.path.exists(inputfile):
-		print "Word embeddings file missing...."
-		exit()
-
-	feature_gen=EmbeddingFeaturesGenerator(inputfile);
-
-	X_train,Y_train=feature_gen.extract_features(test_set)
+	X_train,Y_train=feature_gen.extract_features(train_set)
 	X_test,Y_test=feature_gen.extract_features(test_set)
+
+	print "-------- Training SVM --------";
 
 	classifier = SVC()
 	classifier.fit(X_train, np.asarray(Y_train))
 	
+	print "-------- Predicting --------";
+
 	predicted=classifier.predict(X_test)
-		
+
+	print "-------- Classification Results --------";
 	print(classification_report(Y_test, predicted))
 	print(accuracy_score(Y_test, predicted))
 	print(confusion_matrix(Y_test, predicted))
 
 
 if __name__ == "__main__":
-   main()
+
+	parser = argparse.ArgumentParser(description='Text-based classification using word-embedding representation')
+	parser.add_argument('--train_set', metavar='path', required=True, help='File containing sentences to be used for training')
+	parser.add_argument('--test_set', metavar='path', required=True, help='File containing sentences to be used for testing')
+	parser.add_argument('--embeddings_file', metavar='path', required=False, help='Text file containing pre-trained word embeddings')
+	args = parser.parse_args()
+	main(train_set=args.train_set,test_set=args.test_set,embeddings_file=args.embeddings_file)
